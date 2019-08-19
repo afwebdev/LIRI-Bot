c9e1d09828c090af45136db48c590668d5e85c6d
AFWebDev <andrewfariaj@gmail.com>
2019-08-18 20:39:30 -0400

    spotify added

--- liri.js
+++ liri.js
@@ -1,3 +1,68 @@
+require("dotenv").config();
+
 let moment = require("moment");
-let spotify = require("node-spotify-api");
-let 
+let Spotify = require("node-spotify-api");
+let keys = require("./keys.js");
+let inquirer = require("inquirer");
+let axios = require("axios");
+
+let spotify = new Spotify(keys.spotify);
+
+const searchPrompt = () => {
+	inquirer
+		.prompt([
+			{
+				type: "list",
+				name: "option",
+				message: "Welcome to LIRI-bot, a multi-use entertainment search",
+				choices: ["Find a Song", "Find a Concert", "Find a Movie", "RANDOM"]
+			},
+			{
+				type: "input",
+				name: "query",
+				message: "Search for something:"
+			}
+		])
+		.then(answers => {
+			switch (answers.query) {
+				case "Find a Song":
+					searchSpotify(answers.query);
+					break;
+				case "Find a Concert":
+					break;
+				case "Find a Movie":
+					break;
+				case "RANDOM":
+					break;
+				default:
+					break;
+			}
+			console.log(answers.option);
+			searchSpotify(answers.query);
+		});
+};
+
+const searchSpotify = query => {
+	spotify.search(
+		{
+			type: "track",
+			query: query
+		},
+		(err, data) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			let title = data.tracks.items[0].name;
+			let artist = data.tracks.items[0].album.artists[0].name;
+			let album = data.tracks.items[0].album.name;
+			//Track title
+			console.log(`* Title: ${title}`);
+			console.log(`* Artist: ${artist}`);
+			console.log(`* Album: ${album}`);
+			console.log("********************************");
+		}
+	);
+};
+
+searchPrompt();
