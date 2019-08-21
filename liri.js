c379eb14165d84b2d99be7d48fc480cb16d8753e
AFWebDev <andrewfariaj@gmail.com>
2019-08-20 22:28:40 -0400

    convert to class based constructor, proto functions

--- liri.js
+++ liri.js
@@ -1,21 +1,13 @@
 require("dotenv").config();
 
 let moment = require("moment");
-let Spotify = require("node-spotify-api");
 let keys = require("./keys.js");
 let inquirer = require("inquirer");
-let axios = require("axios");
 let fs = require("fs");
 
-let spotify = new Spotify(keys.spotify);
-
-const concertSearch = (query, prompt) => {
-	let url = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
-	axios.get(url).then(data => {
-		console.log(data);
-		prompt(true);
-	});
-};
+let Search = require("./search.js");
+let search = new Search();
+let divider = "********************************";
 
 const readFromFile = () => {
 	fs.readFile("./random.txt", "utf8", (err, file) => {
@@ -28,13 +20,13 @@ const readFromFile = () => {
 
 		switch (type) {
 			case "spotify-this-song":
-				searchSpotify(query, searchPrompt);
+				search.spotify(query, searchPrompt);
 				break;
 			case "concert-this":
-				concertSearch(query, searchPrompt);
+				search.concert(query, searchPrompt);
 				break;
 			case "movie-this":
-				searchOMDB(query, searchPrompt);
+				search.omdb(query, searchPrompt);
 				break;
 			case "RANDOM":
 				readFromFile();
@@ -42,39 +34,8 @@ const readFromFile = () => {
 			default:
 				break;
 		}
+		// searchPrompt();
 	});
-};
-
-const searchOMDB = (query, prompt) => {
-	if (query === "") {
-		query = "Mr.Nobody";
-	}
-	let key = keys.omdbKey.key;
-	let url = `http://www.omdbapi.com/?apikey=${key}&t=${query}`;
-	axios
-		.get(url)
-		.then(response => {
-			const rd = response.data;
-
-			const { Title, Year, Plot, Country, Ratings, Language, Actors, Released } = response.data;
-
-			console.log("********************************");
-			console.log(`* Title: ${Title}`);
-			console.log(`* Year: ${Year}`);
-			console.log(`* Released: ${Released}`);
-			console.log(`* Plot: ${Plot}`);
-			console.log(`* Actors: ${Actors}`);
-			console.log(`* Actors: ${Language}`);
-			console.log(`* Country: ${Country}`);
-			Ratings.forEach(rating => {
-				console.log("* Ratings: " + rating.Source + " -> " + rating.Value);
-			});
-			console.log("**********************************");
-			prompt(true);
-		})
-		.catch(err => {
-			console.error(err);
-		});
 };
 
 const searchPrompt = searchAgain => {
@@ -93,25 +54,26 @@ const searchPrompt = searchAgain => {
 				choices: ["Find a Song", "Find a Concert", "Find a Movie", "RANDOM"]
 			},
 			{
-				// when: function(answers) {
-				// 	answers.option !== "RANDOM" ? true : false;
-				// },
+				when: function(answers) {
+					if (answers.option !== "RANDOM") {
+						return true;
+					}
+				},
 				type: "input",
 				name: "query",
 				message: "Search for something:"
 			}
 		])
 		.then(answers => {
-			console.log(answers.option);
 			switch (answers.option) {
 				case "Find a Song":
-					searchSpotify(answers.query, searchPrompt);
+					search.spotify(answers.query, searchPrompt);
 					break;
 				case "Find a Concert":
-					concertSearch(answers.query, searchPrompt);
+					search.concert(answers.query, searchPrompt);
 					break;
 				case "Find a Movie":
-					searchOMDB(answers.query, searchPrompt);
+					search.omdb(answers.query, searchPrompt);
 					break;
 				case "RANDOM":
 					readFromFile(searchPrompt);
@@ -121,33 +83,6 @@ const searchPrompt = searchAgain => {
 			}
 			//
 		});
-};
-
-const searchSpotify = (query, prompt) => {
-	spotify.search(
-		{
-			type: "track",
-			query: query
-		},
-		(err, data) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
-			//success,
-			let title = data.tracks.items[0].name;
-			let artist = data.tracks.items[0].album.artists[0].name;
-			let album = data.tracks.items[0].album.name;
-			//Log Track info
-			console.log("********************************");
-			console.log(`* Title: ${title}`);
-			console.log(`* Artist: ${artist}`);
-			console.log(`* Album: ${album}`);
-			console.log("********************************");
-
-			prompt(true);
-		}
-	);
 };
 
 searchPrompt();
--- search.js
+++ search.js
@@ -1,5 +1,114 @@
-const Search = function(query) {
-	this.query = query;
+require("dotenv").config();
+let keys = require("./keys.js");
+let Spotify = require("node-spotify-api");
+let axios = require("axios");
+let moment = require("moment");
+let divider = "*********************************************************";
+class Search {
+	constructor(query) {
+		this.query = query;
+	}
+}
+
+//Spotify API Search
+Search.prototype.spotify = function(query, callback) {
+	console.clear();
+	let spotify = new Spotify(keys.spotify);
+
+	spotify.search(
+		{
+			type: "track",
+			query: query
+		},
+		(err, data) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			//success,
+			let title = data.tracks.items[0].name;
+			let artist = data.tracks.items[0].album.artists[0].name;
+			let album = data.tracks.items[0].album.name;
+			//Log Track info
+			console.log(divider);
+			console.log(`* Title: ${title}`);
+			console.log(`* Artist: ${artist}`);
+			console.log(`* Album: ${album}`);
+			console.log(divider);
+			callback(true);
+		}
+	);
 };
 
-Search.prototype.spotify = function() {};
+//OMDB API Search
+Search.prototype.omdb = function(query, callback) {
+	console.clear();
+	if (query === "") {
+		query = "Mr.Nobody";
+	}
+	let key = keys.omdbKey.key;
+	let url = `http://www.omdbapi.com/?apikey=${key}&t=${query}`;
+	axios
+		.get(url)
+		.then(response => {
+			const rd = response.data;
+
+			const { Title, Year, Plot, Country, Ratings, Language, Actors, Released } = response.data;
+
+			console.log(divider);
+			console.log(`* Title: ${Title}`);
+			console.log(`* Year: ${Year}`);
+			console.log(`* Released: ${Released}`);
+			console.log(`* Plot: ${Plot}`);
+			console.log(`* Actors: ${Actors}`);
+			console.log(`* Actors: ${Language}`);
+			console.log(`* Country: ${Country}`);
+			Ratings.forEach(rating => {
+				console.log("* Ratings: " + rating.Source + " -> " + rating.Value);
+			});
+			console.log(divider);
+			callback(true);
+		})
+		.catch(err => {
+			console.error(err);
+		});
+};
+
+//Concert Search
+Search.prototype.concert = (query, callback) => {
+	console.clear();
+
+	let url = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
+	axios
+		.get(url)
+		.then(response => {
+			return response.data;
+		})
+		.then(data => {
+			let totalShows = data.length;
+			console.log(divider);
+			console.log(`${totalShows} results found`);
+			console.log(divider + "\n");
+
+			data.forEach(show => {
+				let { name, city, datetime } = show.venue;
+
+				console.log(divider);
+				console.log(`* Venue Name: ${name}`);
+				console.log(`* Venue Location: ${city}`);
+				console.log(`* Venue Date: ${moment(datetime).format("MM/DD/YYY")}`);
+				console.log(divider + "\n");
+			});
+			callback(true);
+		})
+		.catch(err => {
+			if (err) {
+				console.log(divider);
+				console.log(err.response.data.message);
+				console.log(divider + "\n");
+				callback();
+			}
+		});
+};
+
+module.exports = Search;
